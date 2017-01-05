bb0fb04c306fc3f9328808703a0ee94db0f64bce
bhushanGap91 <bhushangaps@gmail.com>
2017-01-05 13:08:27 +0530

    add the file

---
+++ Java_Tutorials/src/encasulation.java
@@ -0,0 +1,14 @@
+
+public class encasulation {
+
+	   encasulation()
+     {
+   	  System.out.println("Its work constructor");
+     }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+	}
+
+}
